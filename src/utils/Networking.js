be7e784f24e4a1ed4e00efdd1bc31c5bfcc343c8
Teodor Garzdin <tsg@miracle.dk>
2017-03-10 12:39:45 +0100

    Create a custom error class to handle JSON errors. Catch the JSON error in the error handler and refresh token there, then make request again.

--- src/utils/Networking.js
+++ src/utils/Networking.js
@@ -1,3 +1,13 @@
+class JsonError {
+  constructor(json) {
+    this.name = 'JsonError';
+    this.message = json;
+    this.stack = new Error().stack;
+  }
+}
+
+JsonError.prototype = Object.create(Error.prototype);
+
 class Networking {
   static request(route, method = 'GET', body = {}, authenticated = false) {
     const endpoint = "https://monpay.herokuapp.com/api/v1/";
@@ -24,56 +34,52 @@ class Networking {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error();
+          throw new JsonError(response);
         }
       })
       .then(json => {
-        if (json.description) {
-          if (json.description.token) {
-            if (json.description.token.includes("expired")) {
-              fetch(endpoint + "auth/refresh", {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                  token: localStorage.getItem("token"),
-                  refresh_token: localStorage.getItem("refreshToken")
+        resolve(json);
+      })
+      .catch(error => {
+        error.message.json()
+        .then(json => {
+          if (json.description) {
+            if (json.description.token) {
+              if (json.description.token.includes("expired")) {
+                fetch(endpoint + "auth/refresh", {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    token: localStorage.getItem("token"),
+                    refresh_token: localStorage.getItem("refreshToken")
+                  })
                 })
-              })
-              .then(response => {
-                if (response.ok) {
-                  return response.json();
-                } else {
-                  throw new Error();
-                }
-              }).then(json => {
-                localStorage.setItem("token", json.token);
-                localStorage.setItem("refreshToken", json.refresh_token);
-              })
-              .catch(error => {
-                reject(error);
-              });
+                .then(response => {
+                  if (response.ok) {
+                    return response.json();
+                  } else {
+                    throw new Error();
+                  }
+                }).then(json => {
+                  localStorage.setItem("token", json.token);
+                  localStorage.setItem("refreshToken", json.refresh_token);
+                })
+                .catch(error => {
+                  reject(error);
+                });
+              }
             }
           }
-        }
+        });
+      })
+      .then(() => {
         fetch(endpoint + route, method !== 'GET' ? postParams : getParams)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error();
-          }
-        })
+        .then(response => response.json())
         .then(json => {
           resolve(json);
         })
-        .catch(error => {
-          reject(error);
-        });
-      })
-      .catch(error => {
-        reject(error);
       });
     });
   };
